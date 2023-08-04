293f5741e2a8c0a9d786c4f9392aad06e3548094
Sseptimus <69606116+Sseptimus@users.noreply.github.com>
2023-08-04 20:33:31 +1200

    clean project

--- app/App.js
+++ app/App.js
@@ -1,83 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, SafeAreaView } from "react-native";
-// import {MapDisplay} from "./Map.js";
-// import { LeafletView } from "react-native-leaflet";
-
-
-
-/// df
-const MainDisplay = props => {
-  var display = <Text>no display {props.display}</Text>
-  switch (props.display) {
-    case "map":
-      // display = <MapDisplay />
-      break;
-    case "home":
-      display = <Text>home</Text>
-      break;
-  }
-  return (
-    <View style={styles.main}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {display}
-    </View>
-  )
-}
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [display, setDisplay] = useState("home");
   return (
     <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <MainDisplay />
-      <View style={styles.footer}>
-        <Button
-          title="map"
-          onPress={() => {
-            setDisplay("map");
-          }}
-        />
-        <Button
-          title="home"
-          onPress={() => {
-            setDisplay("home");
-          }}
-        />
-      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    justifyContent: "flex-end",
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-    height: "100%",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  footer: {
-    display: "flex",
-    position: "relative",
-    // height: 100,
-    borderTopColor: "#000",
-    borderTopWidth: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    bottom: 0,
-    padding: 20,
-    gap: 20,
-    width: "100%",
-  },
-  main: {
-    display: "flex",
-    alignContent: "center",
-    justifyContent: "center",
-    height: "100%",
-    width: "100%",
-    
-  }
 });
