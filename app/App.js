099621d1ac8238467169b769d4d6d78e0e14de55
ollielynas <lynasollie@gmail.com>
2023-08-04 16:04:01 +1200

    added display managment

--- app/App.js
+++ app/App.js
@@ -1,27 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import {MapDisplay} from "./app/Map.js";
 
 
-const MainDisplay = () => {
+
+/// df
+const MainDisplay = props => {
+  var display = <Text>no display {props.display}</Text>
+  switch (props.display) {
+    case "map":
+      display = <MapDisplay />
+      break;
+    case "home":
+      display = <Text>home</Text>
+      break;
+  }
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
-      
+      {display}
     </View>
   )
 }
 
 export default function App() {
+  let display = "home"
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MainDisplay  />
       <View id='footer'>
-        <Text>map|home|settings|ect</Text>
+        <Button title="map" onPress={() => {display="map"}} />
+        <Button title="home" onPress={() => {display="home"}} />
+
       </View>
-      <MapDisplay />
     </View>
   );
 }
@@ -38,5 +52,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#caa",
     alignItems: "center",
     justifyContent: "center",
+    
   },
 });
